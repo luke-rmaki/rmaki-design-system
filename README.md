316af5c671acf8677a688a36197865bfab16b77e
Luke Ruokaismaki <luke@rmaki.com.au>
2021-05-12 23:02:03 +1000

    feat: :sparkles: add copy to clipboard on swatches

--- README.md
+++ README.md
@@ -1,33 +1,7 @@
+---
+name: RMAKI
+---
+
 # RMAKI Design System
 
-## Setup
-
-```sh
-yarn # npm i
-```
-
-## Run
-
-```sh
-yarn dev # npm run dev
-```
-
-## Build
-
-```sh
-yarn build # npm run build
-```
-
-## Serve built app
-
-```sh
-yarn serve # npm run serve
-```
-
-## Deploy
-
-```sh
-yarn deploy
-```
-
-Note that by default `docz` generates the output site in `.docz/public` to change that add a `dest` field to your `doczrc.js` with the path you want to generate the code in.
+[Colors](src-colors)
---
+++ src/utils/components/Swatch.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable no-new */
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
+
+import { hex_converter } from '../functions/hex_converter';
+
+const ClipboardJS = require(`clipboard`);
+
+type Props = {
+  hex: string;
+};
+
+export const Swatch = (props: Props) => {
+  const { hex }: { hex: string } = props;
+
+  const rgb = hex_converter(`#${hex}`);
+
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  new ClipboardJS(`#copy_${hex}`, {
+    text: () => {
+      setShowTooltip(true);
+      setTimeout(() => {
+        setShowTooltip(false);
+      }, 2000);
+      return `#${hex}`;
+    },
+  });
+
+  new ClipboardJS(`#copy_rgb_${hex}`, {
+    text: () => {
+      setShowTooltip(true);
+      setTimeout(() => {
+        setShowTooltip(false);
+      }, 2000);
+      return rgb;
+    },
+  });
+
+  return (
+    <StyledSwatch color={hex} showTip={showTooltip}>
+      <div />
+      <span>
+        <input defaultValue={`#${hex}`} />
+        <button type="button" id={`copy_${hex}`}>
+          <FontAwesomeIcon icon={faClipboard} />
+          <span>Copied</span>
+        </button>
+      </span>
+      <span>
+        <input defaultValue={`${rgb}`} />
+        <button type="button" id={`copy_rgb_${hex}`}>
+          <FontAwesomeIcon icon={faClipboard} />
+        </button>
+      </span>
+    </StyledSwatch>
+  );
+};
+
+type StyledProps = {
+  color: string;
+  showTip: boolean;
+};
+
+const StyledSwatch = styled.li<StyledProps>`
+  text-align: center;
+  margin-bottom: 50px;
+
+  div {
+    background-color: #${(props) => props.color};
+    height: 100px;
+    width: 200px;
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  span {
+    display: flex;
+    width: 100%;
+    margin-top: 10px;
+    height: 25px;
+
+    input {
+      flex: 1 0;
+      text-align: center;
+      border: none;
+      border-radius: 5px 0 0 5px;
+    }
+
+    button {
+      border: none;
+      border-radius: 0 5px 5px 0;
+      cursor: pointer;
+      width: 20px;
+      position: relative;
+
+      span {
+        position: absolute;
+        background-color: #fff;
+        bottom: 120%;
+        width: 50px;
+        left: -25px;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        transition: all 0.5s ease-in;
+        opacity: ${(props) => (props.showTip ? 1 : 0)};
+        transform: translateY(${(props) => (props.showTip ? `0px` : `10px`)});
+      }
+    }
+  }
+`;
